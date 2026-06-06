ea2c45981262ccfde12c592cc07c05b4932426d5
Raul Correa <raulcorreainformatica@gmail.com>
2026-06-06 20:28:42 +0100

    Eliminados archivos antiguos

--- trabajo/src/main/java/com/biblioteca/trabajo/repository/autorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.biblioteca.trabajo.repository;
-
-import com.biblioteca.trabajo.model.autor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface autorRepository extends JpaRepository<autor, Long> {
-}
--- trabajo/src/main/java/com/biblioteca/trabajo/repository/categoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.biblioteca.trabajo.repository;
-
-import com.biblioteca.trabajo.model.categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface categoriaRepository extends JpaRepository<categoria, Long> {
-
-    }
-
